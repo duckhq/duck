2d1f798c990209926cc717c997c28d77dd14c463
Patrik Svensson <patrik@patriksvensson.se>
2020-01-22 15:58:30 +0100

    Make provider icons stand out more
    Also adds a slight shadow to build cards.

--- web/src/components/Build.vue
+++ web/src/components/Build.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-card :bg-variant="getBackground(build)" text-variant="light" tag="article">
+  <b-card :bg-variant="getBackground(build)" text-variant="light" tag="article" style="box-shadow:0px 0px 5px 5px rgba(0,0,0,0.2)">
     <template v-slot:header>
       <h6 class="mb-0">{{ build.project }}</h6>
       <span>{{ build.build }}</span>
@@ -31,37 +31,38 @@
         <tr>
           <td>
             <span v-if="build.status != 'Running'" class="small">{{ getBuildStatus(build) }}</span>
-            <span v-if="build.status != 'Running'" class="small"> {{ build.finished | moment("from", "now") }}</span>
+            <span
+              v-if="build.status != 'Running'"
+              class="small"
+            >{{ build.finished | moment("from", "now") }}</span>
             <table v-if="build.status == 'Running'">
               <tr>
-                <td><pulse-loader color="white" size="8px" class="span" /></td>
-                <td style="padding-left:5px;"><span class="small"> {{ build.started | moment("from", "now") }}</span></td>
+                <td>
+                  <pulse-loader color="white" size="8px" class="span" />
+                </td>
+                <td style="padding-left:5px;">
+                  <span class="small">{{ build.started | moment("from", "now") }}</span>
+                </td>
               </tr>
             </table>
           </td>
           <td style="width:auto;text-align:right;white-space: nowrap">
             <a :href="build.url" target="_blank" title="Go to build">
-            <img
-              v-if="build.provider == 'TeamCity'"
-              src="../assets/teamcity.svg"
-              style="opacity: 0.7;"
-              width="28"
-              height="28"
-            />
-            <img
-              v-if="build.provider == 'AzureDevOps'"
-              src="../assets/azuredevops.svg"
-              style="opacity: 0.7;"
-              width="28"
-              height="28"
-            />
-            <img
-              v-if="build.provider == 'OctopusDeploy'"
-              src="../assets/octopus.svg"
-              style="opacity: 1.0;"
-              width="28"
-              height="28"
-            />
+              <img
+                v-if="build.provider == 'TeamCity'"
+                src="../assets/teamcity.svg"
+                class="build-provider-avatar"
+              />
+              <img
+                v-if="build.provider == 'AzureDevOps'"
+                src="../assets/azuredevops.svg"
+                class="build-provider-avatar"
+              />
+              <img
+                v-if="build.provider == 'OctopusDeploy'"
+                src="../assets/octopus.svg"
+                class="build-provider-avatar"
+              />
             </a>
           </td>
         </tr>
@@ -99,3 +100,18 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.build-provider-avatar {
+  opacity: 1;
+  padding: 3px;
+  border: 1px;
+  border-style: solid;
+  border-color: gray;
+  border-radius: 5px;
+  width: 32px;
+  height: 32px;
+  background: #fdfdfd;
+  box-shadow:0px 0px 3px 3px rgba(0,0,0,0.15);
+}
+</style>
