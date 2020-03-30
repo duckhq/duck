3a194a2762e7ccd8286611cf9f35cd59e97fd968
Patrik Svensson <patrik@patriksvensson.se>
2020-03-30 14:17:05 +0200

    Add animations in frontend

--- web/src/components/BuildList.vue
+++ web/src/components/BuildList.vue
@@ -16,5 +16,29 @@ export default {
 </script>
 
 <style scoped>
-
+#app .build-enter-active,
+#app .build-leave-active {
+  transition: 1s cubic-bezier(0.59, 0.12, 0.34, 0.95);
+  transition-property: opacity, transform;
+}
+#app .build-move {
+  transition: transform 1s;
+  transition-property: opacity, transform;
+}
+#app .build-enter {
+  opacity: 0;
+  transform: translateX(125px) scaleY(1) scaleX(1);
+}
+#app .build-enter-to {
+  opacity: 1;
+  transform: translateX(0) scaleY(1);
+}
+#app .build-leave-active {
+  position: absolute;
+}
+#app .build-leave-to {
+  opacity: 0;
+  transform: translateX(0);
+  transform-origin: center top;
+}
 </style>
