a48d607f1e97c050383d3086ef9f17d822c879a7
Gary McLean Hall <gary@adaptivecode.com>
2020-04-05 23:22:14 +0100

    Integrate purgecss (#59)
    Integrated PurgeCSS to eradicate unused CSS.
    Shrinks the size of CSS output considerably.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,24 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.js(x)',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
-  plugins: {
-    tailwindcss: './tailwind.config.js',
-    autoprefixer: {}
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
 }
