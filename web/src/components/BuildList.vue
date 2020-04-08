ddd2d032a4d206b4da3c6c427e4b919a7c52575e
Gary McLean Hall <gary@adaptivecode.com>
2020-04-08 12:58:35 +0100

    Created skeleton loading cards to show some content while builds are loading

--- web/src/components/BuildList.vue
+++ web/src/components/BuildList.vue
@@ -1,42 +1,77 @@
 <template>
-  <transition-group name="build" tag="div" class="grid md:grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <Build v-for="build in builds" :key="build.id" :build="build" />
+  <div :key="skeleton" v-if="loading" class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div v-for="build in getBuilds" :key="build.id" >
+      <SkeletonBuild class="skeleton" /> 
+    </div>
+  </div>
+  <div v-else :key="real">
+    <transition-group name="build" tag="div" class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div v-for="build in getBuilds" :key="build.id">
+        <Build :build="build" />
+    </div>
   </transition-group>
+  </div>
 </template>
 
 <script>
 import Build from "./Build.vue";
+import SkeletonBuild from "./SkeletonBuild.vue";
 
 export default {
-  props: ["builds"],
+  props: {
+    builds: {
+      type: Array
+    }
+  },  
   components: {
-    Build
+    Build,
+    SkeletonBuild
+  },
+  methods: {
+    skeletonBuilds: function() {
+      return [...Array(6).keys()].map(id => ({
+        id,
+        isSkeleton: true
+      }));
+    }
+  },
+  computed: {
+    loading: function() {
+      return this.builds == null;
+    },
+    getBuilds: function() {
+      const hasRealBuilds = this.builds !== null && this.builds.length > 0;
+      if(hasRealBuilds)
+        return this.builds;
+      else 
+        return this.skeletonBuilds();
+    }
   }
 };
 </script>
 
 <style scoped>
-#app .build-enter-active,
-#app .build-leave-active {
+.build-enter-active,
+.build-leave-active {
   transition: 1s cubic-bezier(0.59, 0.12, 0.34, 0.95);
   transition-property: opacity, transform;
 }
-#app .build-move {
+.build-move {
   transition: transform 1s;
   transition-property: opacity, transform;
 }
-#app .build-enter {
+.build-enter {
   opacity: 0;
   transform: translateX(125px) scaleY(1) scaleX(1);
 }
-#app .build-enter-to {
+.build-enter-to {
   opacity: 1;
   transform: translateX(0) scaleY(1);
 }
-#app .build-leave-active {
+.build-leave-active {
   position: absolute;
 }
-#app .build-leave-to {
+.build-leave-to {
   opacity: 0;
   transform: translateX(0);
   transform-origin: center top;
